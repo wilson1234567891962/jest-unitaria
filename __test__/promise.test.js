853018e14c96e6d9571f375336a7927beb2e84df
Sem - 6 - INGENIERIA INDUSTRIAL: Wilson Alejandro Gonzalez Gaitan <wilsongonzalez.ag@academia.umb.edu.co>
2019-06-11 20:46:07 -0500

    Implements test with reject and resolve

--- __test__/promise.test.js
+++ __test__/promise.test.js
@@ -2,11 +2,19 @@ import { getDataFromApi } from '../promise';
 
 describe('Probando promesas', () => {
 
-    test('Realizando una prueba de promesa con api', done => {
+    test('Realizando una peticion a una api', done => {
         const api = 'https://rickandmortyapi.com/api/character/';
         getDataFromApi(api).then(data => {
-            expect(data.results.lenght).toBeGreaterThan(0);
+            expect(data.results.length).toBeGreaterThan(0);
             done();
         });
+    });
+
+    test('Resuelve un Hola!', () => {
+        return expect(Promise.resolve('Hola!')).resolves.toBe('Hola!');
+    });
+
+    test('Rechaza con un error', () => {
+        return expect(Promise.reject('Errror')).rejects.toBe('Errror');
     });
 });
