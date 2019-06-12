5f4761fd9fdccfe9769318160b64d04f4f2492d6
Sem - 6 - INGENIERIA INDUSTRIAL: Wilson Alejandro Gonzalez Gaitan <wilsongonzalez.ag@academia.umb.edu.co>
2019-06-11 21:55:56 -0500

    Create test with exception at snapshot

--- __test__/snapshot.test.js
+++ __test__/snapshot.test.js
@@ -5,5 +5,23 @@ describe('test de snapshot', () => {
     test('Snapshot', () => {
         expect(getCharacter(rick)).toMatchSnapshot();
     });
-    
+
+    test('Test de snapshot para fallar test', () => {
+        const user = {
+            createAt: new Date(),
+            id: Math.floor(Math.random() * 20),
+        };
+        expect(user).toMatchSnapshot();
+    });
+
+    test('Excepcion de test del snapshot', () => {
+        const user = {
+            name: 'Alejandro Gonzalez',
+            id: Math.floor(Math.random() * 20),
+        };
+        expect(user).toMatchSnapshot({
+            id: expect.any(Number)
+        });
+    });
+
 });
